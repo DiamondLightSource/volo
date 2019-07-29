f6e8503eb470036c1f09074ea49b405ac5e53e90
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2019-07-29 18:31:05 +0100

    A working optimizer?

--- lov/optimizer.py
+++ lov/optimizer.py
@@ -1,54 +1,104 @@
+import at
 import numpy
 from scipy.optimize import least_squares
 
 
 class Variables(object):
-    def __init__(fields, indices, values):
-        if not(len(fields) == len(indices) == len(values)):
-            raise IndexError("Length mismatch: Lists of fields({0}), "
-                             "indices({1}), and values({2}) must be the same "
-                             "length.".format(len(fields), len(indices),
-                                              len(values)))
+    def __init__(self, fields, indices, values):
+        """Variable fields should either be a field name as a string e.g.
+        'Length' or a field name(string) and a cell(int) e.g. ['PolynomB', 2].
+        To change mutiple fields on the same element, multiple field index
+        pairs must be passed.
+        """
+        if (len(fields) != len(indices)) or (len(fields) != len(values)):
+            raise IndexError("Length mismatch: Lengths of fields ({0}), "
+                             "indices ({1}), and values ({2}) are not equal."
+                             .format(len(fields), len(indices), len(values)))
         self.fields = fields
         self.indices = indices
         self.initial_values = values
 
-class Constraints(object):
-    def __init__(lattice, weights, refpts, constraints):
-        self.lattice = lattice
-        self.weightings = [
-            weights['tune_x'], weights['tune_y'],
-            weights['chrom_x'], weights['chrom_y'],
-            weights['alpha_x'], weights['alpha_y'],
-            weights['beta_x'], weights['beta_y'],
-            weights['mu_x'], weights['mu_y'],
-            weights['x'], weights['px'], weights['y'], weights['py'],
-            weights['dispersion'], weights['gamma']
-        ]
-        self.refpts = refpts
-        self.desired_constraints = constraints
 
-    def calc_lindata():
+class Constraints(object):
+    def __init__(self, lattice, weights, constraints):
+        """weights is a dictionary of format:
+            {field: weight}
+        constraints is a dictionary of format:
+            {field: [[refpts], [desired_values]]}
+        where refpts is an ordered ascending list of integers, and
+        desired_values is a list of the corresponding goal values for that
+        field at refpts.
+        """
+        self.lattice = lattice
+        self.weightings = weights
+        self.desired_constraints = constraints
+        self.refpts = set()
+        for key in constraints.keys():
+            self.refpts.update(constraints[key][0])
+        self.refpts = list(self.refpts)
+        """
+        weights.get('tune_x', 0), weights.get('tune_y', 0),
+        weights.get('chrom_x', 0), weights.get('chrom_y', 0),
+        weights.get('alpha_x', 0), weights.get('alpha_y', 0),
+        weights.get('beta_x', 0), weights.get('beta_y', 0),
+        weights.get('mu_x', 0), weights.get('mu_y', 0),
+        weights.get('x', 0), weights.get('px', 0),
+        weights.get('y', 0), weights.get('py', 0),
+        weights.get('dispersion', 0), weights.get('gamma', 0)
+        self.desired_values = []
+        self.desired_values.extend(constraints.get('tune_x', [[],[]])[1])
+        self.desired_values.extend(constraints.get('tune_y', [[],[]])[1])
+        self.desired_values.extend(constraints.get('chrom_x', [[],[]])[1])
+        self.desired_values.extend(constraints.get('chrom_y', [[],[]])[1])
+        self.desired_values.extend(constraints.get('alpha_x', [[],[]])[1])
+        self.desired_values.extend(constraints.get('alpha_y', [[],[]])[1])
+        self.desired_values.extend(constraints.get('beta_x', [[],[]])[1])
+        self.desired_values.extend(constraints.get('beta_y', [[],[]])[1])
+        self.desired_values.extend(constraints.get('mu_x', [[],[]])[1])
+        self.desired_values.extend(constraints.get('mu_y', [[],[]])[1])
+        self.desired_values.extend(constraints.get('x', [[],[]])[1])
+        self.desired_values.extend(constraints.get('px',[[],[]])[1])
+        self.desired_values.extend(constraints.get('y', [[],[]])[1])
+        self.desired_values.extend(constraints.get('py',[[],[]])[1])
+        self.desired_values.extend(constraints.get('dispersion', [[],[]])[1])
+        self.desired_values.extend(constraints.get('gamma', [[],[]])[1])
+        """
+
+    def calc_lindata(self):
         return at.linopt(self.lattice, refpts=self.refpts, get_chrom=True,
                          coupled=False)
 
-    def convert_lindata(lindata):
+    def convert_lindata(self, lindata):
         """Order : [tune_x, tune_y, chrom_x, chrom_y, alpha_x, alpha_y, beta_x,
                     beta_y, mu_x, mu_y, x, px, y, py, dispersion, gamma]
+        Emittance could possibly be added, though it would increase the
+        calculation time dramatically.
+        dispersion ?is (4,)?
         """
-        d = []  # How do we propgate to refpts, as merit output must be 1d?
-        d.extend(lindata[1])  # tune_x and tune_y
-        d.extend(lindata[2])  # chrom_x and chrom_y
-        d.extend(lindata[3].alpha)  # alpha_x, alpha_y
-        d.extend(lindata[3].beta)  # beta_x and beta_y
-        d.extend(lindata[3].mu)  # mu_x and mu_y
-        d.extend(lindata[3].closed_orbit[:4])  # x, px, y, and py
-        d.extend(lindata[3].dispersion)  # dispersion ?is (4,)?
-        d.extend(lindata[3].gamma)  # gamma
-        return d
+        data_map = {
+            'tune_x':lindata[1][0], 'tune_y':lindata[1][1],
+            'chrom_x':lindata[2][0], 'chrom_y':lindata[2][1],
+            'alpha_x':lindata[3].alpha[:, 0], 'alpha_y':lindata[3].alpha[:, 1],
+            'beta_x':lindata[3].beta[:, 0], 'beta_y':lindata[3].beta[:, 1],
+            'mu_x':lindata[3].mu[:, 0], 'mu_y':lindata[3].mu[:, 1],
+            'x':lindata[3].closed_orbit[:, 0],
+            'px':lindata[3].closed_orbit[:, 1],
+            'y':lindata[3].closed_orbit[:, 2],
+            'py':lindata[3].closed_orbit[:, 3],
+            'dispersion':lindata[3].dispersion, 'gamma':lindata[3].gamma
+        }
+        data = {}
+        for key in self.desired_constraints.keys():
+            if key in ['tune_x', 'tune_y', 'chrom_x', 'chrom_y']:
+                d = [data_map[key]]
+            else:
+                d = [data_map[key][self.refpts.index(ref)] for ref
+                     in self.desired_constraints[key][0]]
+            data[key] = numpy.array(d)
+        return data
 
-    def make_changes(values, variables):
-        for v, f, i in zip(value, variables.fields, variables.indices):
+    def make_changes(self, values, variables):
+        for v, f, i in zip(values, variables.fields, variables.indices):
             if isinstance(f, str):
                 vars(self.lattice[i])[f] = v
             else:
@@ -56,20 +106,23 @@ class Constraints(object):
                 cell = f[1]
                 vars(self.lattice[i])[field][cell] = v
 
-    def merit_function(values, variables):
+    def merit_function(self, values, variables):
         self.make_changes(values, variables)
-        constraints = self.convert_lindata(calc_lindata())
-        diff = constraints - self.desired_constraints
-        return diff * self.weightings
+        constraints = self.convert_lindata(self.calc_lindata())
+        residuals = []
+        for key in self.desired_constraints.keys():
+            diff = constraints[key] - self.desired_constraints[key][1]
+            residuals.extend(diff * self.weightings[key])
+        return residuals
 
 
 class Optimizer(object):
-    def __init__():
+    def __init__(self, c, v):
         # create variables and constraints objects
-        self.cons = Constraints()
-        self.vars = Variables()
+        self.cons = c
+        self.vars = v
 
-    def run(max_iters=None, verbosity=0):
+    def run(self, max_iters=None, verbosity=0):
         least_squares(self.cons.merit_function, self.vars.initial_values,
-                      max_nfev=max_iters, verbose=verbosity, args=(self.vars))
+                      max_nfev=max_iters, verbose=verbosity, args=([self.vars]))
         return self.cons.lattice
